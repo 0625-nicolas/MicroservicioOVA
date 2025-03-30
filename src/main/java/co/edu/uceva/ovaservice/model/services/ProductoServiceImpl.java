49cb6bf07bc3907d494fcfaf8c796dc0b01f937e
0625-nicolas <nicolas.gutierrez01@uceva.edu.co>
2025-03-30 11:21:51 -0500

    feat: Implementacion de la interfaz del servicio de producto
     Esta clase implementa la interfaz y es necesaria para poder realizar la inyeccion de dependencias en los controladores REST o en donde sea necesario. Los metodos implementados hacen posible realizar el CRUD usando el repositorio del producto.

--- src/main/java/co/edu/uceva/ovaservice/model/services/ProductoServiceImpl.java
+++ src/main/java/co/edu/uceva/ovaservice/model/services/ProductoServiceImpl.java
@@ -1,4 +1,39 @@
 package co.edu.uceva.ovaservice.model.services;
 
-public class ProductoServiceImpl {
+
+import co.edu.uceva.ovaservice.model.entities.Producto;
+import co.edu.uceva.ovaservice.model.repositories.IProductoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductoServiceImpl implements IProductoService {
+
+    IProductoRepository productoRepository;
+
+    public ProductoServiceImpl(IProductoRepository productoRepository) {this.productoRepository = productoRepository;}
+
+    @Override
+    public Producto save(Producto producto) {return productoRepository.save(producto); }
+
+    @Override
+    public void delete(Producto producto) {
+        productoRepository.delete(producto);
+    }
+
+    @Override
+    public Producto findById(Long id) {
+        return productoRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Producto update(Producto producto) {
+        return productoRepository.save(producto);
+    }
+
+    @Override
+    public List<Producto> findAll() {
+        return (List<Producto>) productoRepository.findAll();
+    }
 }
