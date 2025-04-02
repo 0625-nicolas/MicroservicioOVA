8b7831b909962002829d9abef3fdc0e6aa2f9647
0625-nicolas <nicolas.gutierrez01@uceva.edu.co>
2025-04-02 01:32:44 -0500

    feat: Se agregaron validaciones a save y update de entidad Producto

--- src/main/java/co/edu/uceva/ovaservice/controllers/OVARestController.java
+++ src/main/java/co/edu/uceva/ovaservice/controllers/OVARestController.java
@@ -2,6 +2,7 @@ package co.edu.uceva.ovaservice.controllers;
 
 import co.edu.uceva.ovaservice.model.entities.OVA;
 import co.edu.uceva.ovaservice.model.services.IOVAService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.data.domain.Page;
@@ -9,6 +10,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -86,8 +88,19 @@ public class OVARestController {
      * Crear un nuevo ova pasando el objeto en el cuerpo de la petición.
      */
     @PostMapping("/ovas")
-    public ResponseEntity<Map<String, Object>> save(@RequestBody OVA ova) {
+    public ResponseEntity<Map<String, Object>> save(@Valid @RequestBody OVA ova , BindingResult result) {
         Map<String, Object> response = new HashMap<>();
+
+        if (result.hasErrors()) {
+            List<String> errors = result.getFieldErrors()
+                    .stream()
+                    .map(err -> "El campo '" + err.getField() + "' " + err.getDefaultMessage())
+                    .toList();
+
+            response.put("errors", errors);
+            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        }
+
 
         try {
             // Guardar el ova en la base de datos
@@ -133,8 +146,18 @@ public class OVARestController {
      * @param ova: Objeto OVA que se va a actualizar
      */
     @PutMapping("/ovas")
-    public ResponseEntity<Map<String, Object>> update(@RequestBody OVA ova) {
+    public ResponseEntity<Map<String, Object>> update(@RequestBody OVA ova , BindingResult result) {
         Map<String, Object> response = new HashMap<>();
+
+        if (result.hasErrors()) {
+            List<String> errors = result.getFieldErrors()
+                    .stream()
+                    .map(err -> "El campo '" + err.getField() + "' " + err.getDefaultMessage())
+                    .toList();
+
+            response.put("errors", errors);
+            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        }
 
         try {
             // Verificar si el ova existe antes de actualizar
